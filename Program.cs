ef7fe3bc8416dc40cf78ccc82e8b306f7b84a397
Shawn S <Shawn S@DESKTOP-QFV6UHA>
2021-09-02 14:18:05 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordBot_Dragon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
